f835584148d8411bd551ee515a41888ad670b729
Limax <limax10000100100000@gmail.com>
2021-01-26 02:10:15 +0100

    CreateEvent

--- app/controllers/AndroidController.java
+++ app/controllers/AndroidController.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import org.hibernate.Hibernate;
+import play.db.jpa.JPA;
 import play.mvc.*;
 import java.util.*;
 import models.*;
@@ -33,7 +34,7 @@ public class AndroidController extends Controller
             u = User.find("byEmail", email).first();
             if (u == null)
             {
-                User newUser = new User(fullname,email,username, password);
+                User newUser = new User(username,email,fullname, password);
                 newUser.save();
                 renderText("OK");
             }
@@ -193,6 +194,86 @@ public class AndroidController extends Controller
                 }
                 else
                     renderText("EXIST");
+
+            } else {
+                renderText("FAIL: WRONG PASSWORD");
+            }
+        }
+    }
+
+
+    public static void CreateEvent()
+    {
+        String username = params.get("user");
+        String password = params.get("password");
+        String name = params.get("name");
+        String description = params.get("description");
+        Date startDate = new Date(params.get("startDate"));
+        Date endDate = new Date(params.get("endDate"));
+        String addressPhysical = params.get("addressPhysical");
+        String addressOnline = params.get("addressOnline");
+        String calName = params.get("calName");
+
+        User u = User.find("byUsername", username).first();
+
+        if (u == null) {
+
+            renderText("FAIL: NO USER");
+        } else {
+            if (u.password.equals(password))
+            {
+
+                //Obtenim les dates en el format correcte i evitem errors de dates
+                try {
+
+                    if(name.isEmpty()||params.get("startDate").isEmpty()||params.get("endDate").isEmpty()||calName.isEmpty())
+                    {
+                        renderText("EMPTY-VARIABLES");
+                    }
+                    else if (startDate.getTime() >= endDate.getTime()) //Evita dates on la data d'inici i final estàn canviats o són iguals
+                    {
+                        renderText("NO-TIME-EVENT");
+                    }
+                    else if(startDate.getTime() + 84400000 < endDate.getTime())
+                    {
+                        renderText("MORE-THAN-1DAY");
+                    }
+                    else
+                        {
+                            LinCalendar calendar;
+                            for (LinCalendar cal : u.ownedCalendars) {
+                                if (cal.calName.equals(calName)) {
+                                    calendar = cal;
+                                    CalEvent event= new CalEvent(calendar, name, description, startDate, endDate, addressPhysical, addressOnline );
+                                    event.save();
+                                    calendar.events.add(event);
+                                    calendar.save();
+                                    JPA.em().flush();
+                                    JPA.em().clear();
+
+                                    renderText("OK");
+                                }
+                            }
+
+                            for (Subscription subscription : u.subscriptions) {
+                                if (subscription.calendar.calName.equals(calName)) {
+                                    calendar = subscription.calendar;
+                                    CalEvent event= new CalEvent(calendar, name, description, startDate, endDate, addressPhysical, addressOnline );
+                                    event.save();
+                                    calendar.events.add(event);
+                                    calendar.save();
+                                    JPA.em().flush();
+                                    JPA.em().clear();
+
+                                    renderText("OK");
+                                }
+                            }
+                    }
+                }
+                catch(Exception e)
+                {
+                    renderText("INCORRECT-FORMAT");
+                }
 
             } else {
                 renderText("FAIL: WRONG PASSWORD");
